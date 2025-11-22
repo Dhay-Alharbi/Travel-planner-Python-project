99faa940a8151690fa2171f512bbea8f0bcd6efd
Dhay-Alharbi <141719924+Dhay-Alharbi@users.noreply.github.com>
2025-11-22 20:43:46 +0300

    Added comments

--- data.py
+++ data.py
@@ -1,17 +1,19 @@
 # data.py
+
+#Library
 import pandas as pd
 import os
 
 def clean_data(file_path="travel_data.xlsx", output_path="cleaned_output.xlsx"):
-    """
-    Clean the raw travel data and save it as a new Excel file.
-    """
+    #Check if file exists
     if not os.path.exists(file_path):
-        raise FileNotFoundError(f"Raw data file '{file_path}' not found!")
-
+        #1- Not exists: It will stops and shows an error
+        raise FileNotFoundError(f"The file: '{file_path}' not found")
+    #2- Exists:
+    #load file
     df = pd.read_excel(file_path)
     
-    # Drop unnecessary columns
+    # List of unnecessary columns 
     cols_to_drop = [
         "nightlife",
         "latitude",
@@ -20,7 +22,7 @@ def clean_data(file_path="travel_data.xlsx", output_path="cleaned_output.xlsx"):
         "ideal_durations",
         "avg_temp_monthly"
     ]
-    
+    #Drop columns and save cleaned data
     df = df.drop(columns=cols_to_drop, errors="ignore")
     df.to_excel(output_path, index=False)
     print(f"✅ Cleaned data saved as '{output_path}'")
@@ -28,11 +30,13 @@ def clean_data(file_path="travel_data.xlsx", output_path="cleaned_output.xlsx"):
 
 
 def load_cleaned_data(file_path="cleaned_output.xlsx"):
-    """
-    Load cleaned data. If not found, clean the raw data first.
-    """
-    if not os.path.exists(file_path):
-        print("⚠️ Cleaned data file not found. Cleaning raw data first...")
+    
+    #Check if cleaned data exists 
+    if not os.path.exists(file_path): 
+        #1- Not exists: it  will calls clean_data() to create it
+        print("Cleaned file not found. Creating it now")
         clean_data(output_path=file_path)
+    #2- Exists: it will load cleaned data
     df = pd.read_excel(file_path)
     return df
+
